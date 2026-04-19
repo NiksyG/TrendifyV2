0cf4316be5f6a738bed005b51811abf3aa19ad36
NiksyG <nikola,niksyg@gmail.com>
2026-04-19 13:45:46 +0300

    Next update

---
+++ TrendifyV1.ViewModels/AccountViewModels/ResetPasswordFastViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace TrendifyV1.ViewModels.AccountViewModels
+{
+    public class ResetPasswordFastViewModel
+    {
+        [Required(ErrorMessage = "Имейлът е задължителен.")]
+        [EmailAddress(ErrorMessage = "Невалиден имейл адрес.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Новата парола е задължителна.")]
+        [StringLength(100, ErrorMessage = "Паролата трябва да е поне {2} символа.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Паролите не съвпадат.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ TrendifyV1/Controllers/PasswordController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TrendifyV1.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using TrendifyV1.ViewModels.AccountViewModels;
+
+namespace TrendifyV1.Controllers
+{
+    public class PasswordController(UserManager<ApplicationUser> userManager) : Controller
+    {
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword(ResetPasswordFastViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await userManager.FindByEmailAsync(model.Email);
+
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "Неуспешен опит за смяна на паролата. Проверете данните.");
+                return View(model);
+            }
+
+            var token = await userManager.GeneratePasswordResetTokenAsync(user);
+
+            var result = await userManager.ResetPasswordAsync(user, token, model.NewPassword);
+
+            if (result.Succeeded)
+            {
+                TempData["Success"] = "Паролата беше успешно сменена! Вече можеш да влезеш с новата парола.";
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View(model);
+        }
+    }
+}
